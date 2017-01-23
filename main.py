8a57d5a264d1aa9f96d476c45145478cb70b9af1
Nitesh Mestry <nitesh.mestry87@gmail.com>
2017-01-23 17:45:38 +0530

    updates main.py

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
 print("Hello World")
+print ('She said "don\'t do that"')
+print ("hello" + "nick")
+print ("this costs " + str(6 + 10)  + " bucks")
+
+#Split the string with split function and print a specific element
+print ("New candidate " + "12:Nick:Adams:Male:Student".split(":")[1])
+
+
+#array
+print ("I like " + ["movies", "16", "Python"][2])
+
+#list
+print ({"name": "nick", "age": "27", "hobby": "code"}["hobby"])
+
+
+#variables
+greeting = "hello world"
+print(greeting)
+
+greeting = greeting.split(" ")[0]
+print(greeting)
+print(greeting + " nick")
+
+greeting = greeting + " Nick"
+print(greeting)
+
+
+number = 1
+secondnumber = 2
+print(number * secondnumber + secondnumber * number)
+
+check = True
+print(check)
