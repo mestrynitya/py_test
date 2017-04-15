4989db8631640050840b16836b65e448e1a08ecc
mestrynitya <nitesh@nitesh.com>
2017-04-15 15:43:31 +0530

    arguments.py | how to process arguments in command line

--- arguments.py
+++ arguments.py
@@ -6,3 +6,16 @@ print ("Arguments ", sys.argv)
 ## remove arguments
 sys.argv.remove(sys.argv[0])
 print ("Arguemnts ", sys.argv)
+
+# sum up
+arguments = sys.argv
+sum = 0
+for arg in arguments:
+    try:
+        number = int(arg)
+        sum = sum + number
+
+    except Exception:
+        print ("bad input")
+
+print (sum)
